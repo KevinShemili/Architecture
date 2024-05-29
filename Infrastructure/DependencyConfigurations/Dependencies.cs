85dd0db43be070d08b0bc548a7b575befe3fb15d
Kevin <kevin@dev.al>
2024-05-29 12:33:54 +0200

    fix namespaces

---
+++ Infrastructure/DependencyConfigurations/Dependencies.cs
@@ -0,0 +1,32 @@
+﻿using Application.Contracts.Persistence;
+using Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure.DependencyConfigurations
+{
+    public static class Dependencies
+    {
+
+        public static IServiceCollection AddInfrastructureLayer(this IServiceCollection services, IConfiguration configuration)
+        {
+            ConfigureDatabaseConnection(services, configuration);
+            services.ConfigureScopedServices();
+
+            return services;
+        }
+
+        private static void ConfigureScopedServices(this IServiceCollection services)
+        {
+            services.AddScoped<ICoreDbContext, CoreDbContext>();
+        }
+
+        private static void ConfigureDatabaseConnection(IServiceCollection services, IConfiguration configuration)
+        {
+            var connString = configuration.GetConnectionString("DbConnection");
+            services.AddDbContext<DatabaseContext>(options =>
+                options.UseSqlServer(connString, b => b.MigrationsAssembly("Infrastructure")));
+        }
+    }
+}
--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,5 +1,5 @@
-using Application;
-using Infrastructure;
+using Application.DependencyConfigurations;
+using Infrastructure.DependencyConfigurations;
 using Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 
